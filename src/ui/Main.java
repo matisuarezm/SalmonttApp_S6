50f0b9f9d4eb2d565340c063b258502d94edfd12
matisuarezm <mati.suarez@duocuc.cl>
2025-11-28 21:24:24 -0300

    Actualiza Main.java con Menú

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -3,6 +3,7 @@ package ui;
 import data.GestorUnidades;
 import model.UnidadOperativa;
 
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -17,46 +18,69 @@ public class Main {
         List<UnidadOperativa> unidades = gestor.pruebasManuales();
         List<UnidadOperativa> unidadesTxt = gestor.cargaDesdeTxt("PlantasYCentrosCultivo.txt");
 
-
         do {
-            System.out.println(":::::::: Salmont App ::::::::");
-            System.out.println("1.- Leer unidades pruebas manuales");
-            System.out.println("2.- Leer unidades desde un txt");
-            System.out.println("5.- Salir");
+            System.out.println("====--->>> SALMONTT APP <<<---====");
+            System.out.println("1.- Pruebas Manuales");
+            System.out.println("2.- Pruebas de Archivo Plano");
+            System.out.println("9.- Salir");
+            System.out.print("\nSeleccione una opción del Menú: ");
 
-            opcionMenu = input.nextInt();
-            input.nextLine();
+            try {
+                opcionMenu = input.nextInt();
+                input.nextLine();
 
-            switch (opcionMenu){
-                case 1:
-                    for (UnidadOperativa uni : unidades){
-                        System.out.println(uni.toString() + "\n-----------------------------\n");
-                    }
-                    break;
-                case 2:
-                    for (UnidadOperativa unitxt : unidadesTxt){
-                        System.out.println(unitxt + "\n---------------------\n");
-                    }
-                    break;
-                case 5:
-                    System.out.println("Saliendo, gracias por usar el programa");
-                    break;
-                default:
-                    System.out.println("Opción ingresada invalida, Intente nuevamente");
-                    input.nextLine();
-            }
-
-            if (opcionMenu !=5) {
-                System.out.println("Desea hace alguna otra operación? S/N");
-                String letra = input.nextLine().trim();
-                if (letra.equalsIgnoreCase("N")){
-                    opcionMenu = 5;
-                    System.out.println("Saliendo, gracias por usar el programa");
+                switch (opcionMenu){
+                    case 1:
+                        for (UnidadOperativa uni : unidades){
+                            System.out.println(uni.toString() + "\n-----------------------------\n");
+                        }
+                        break;
+                    case 2:
+                        for (UnidadOperativa unitxt : unidadesTxt){
+                            System.out.println(unitxt + "\n---------------------\n");
+                        }
+                        break;
+                    case 9:
+                        salirSistema();
+                        break;
+                    default:
+                        System.out.println("Opción ingresada invalida, Intente nuevamente");
                 }
+
+                if (opcionMenu >= 1 && opcionMenu <= 2) {
+                    while(true) {
+                        System.out.print("¿Desea hace alguna otra operación? S/N: ");
+                        String letra = input.nextLine().trim().toUpperCase();
+                        if (letra.equals("S") ) {
+                            break;
+                        }else if (letra.equals("N")) {
+                            opcionMenu = 9;
+                            salirSistema();
+                            break;
+                        }else{
+                            System.out.println("Error. Ingrese la letra correspondiente S ó N");
+                        }
+                    }
+                }
+            }catch(InputMismatchException e){
+                System.out.println("Error.. Entrada inválida, Por favor ingrese un numero");
+                input.nextLine();
+                opcionMenu = 0;
             }
-
-
-        }while (opcionMenu != 5);
+        }while (opcionMenu != 9);
         input.close();
+    }
+
+    public static void salirSistema(){
+        System.out.println("Usted esta saliendo del sistema");
+        for (int i = 0; i < 3; i++) {
+            try {
+                Thread.sleep(800);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.print(".");
+        }
+        System.out.println("\nGracias por usar el programa.!!!");
     }
 }
